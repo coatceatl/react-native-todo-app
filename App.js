e9dbd9e608cbf813afbb6237cebe522f4384ee80
Oksana Tkachenko <coatceatl@gmail.com>
2020-04-06 15:08:37 +0300

    added state for addTodo

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar'
 import { AddTodo } from './src/AddTodo'
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title
+      }
+    ])
+  }
+
   return (
     <View>
       <Navbar title='ToDo App' />
       <View style={styles.container}>
-        <AddTodo />
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map(todo => {
+            return <Text key={todo.id}>{todo.title}</Text>
+          })}
+        </View>
       </View>
     </View>
   );
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
-export const AddTodo = props => {
+export const AddTodo = ({ onSubmit }) => {
+  const handleSubmit = () => {
+    onSubmit('Test todo')
+  }
   return (
     <View style={styles.wrapper}>
       <TextInput style={styles.input} />
-      <Button color='#4caf50' title='Add' />
+      <Button color='#4caf50' title='Add' onPress={handleSubmit} />
     </View>
   )
 }
